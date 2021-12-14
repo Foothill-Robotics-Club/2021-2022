2dda788c4e929a4c1718421097bc1880e820d0eb
Arnav G <arnavg@windowslinuxmac.com>
2021-12-13 19:13:38 -0800

    motor buttons

--- v5_template/v5_template-2021-10-19T02-38-25/src/main.cpp
+++ v5_template/v5_template-2021-10-19T02-38-25/src/main.cpp
@@ -16,6 +16,7 @@ using namespace vex;
 vex::controller Controller;
 vex::competition Competition;
 vex::motor LeftMotor = vex::motor(PORT1);
+vex::motor RightMotor = vex::motor(PORT2);
 
 void intake(){
 
@@ -25,6 +26,22 @@ int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
 
-  LeftMotor.spin(vex::directionType::fwd, (Controller.Axis3.value() + Controller.Axis1.value()*2), vex::velocityUnits::pct);//left motor will spin forward and change direction according to input from the right stick
-  
+  //LeftMotor.spin(vex::directionType::fwd, (Controller.Axis3.value() + Controller.Axis1.value()*2), vex::velocityUnits::pct);//left motor will spin forward and change direction according to input from the right stick
+    while(1){
+      if (Controller.ButtonX.pressing()){
+        LeftMotor.spin(vex::directionType::rev, 30, vex::velocityUnits::pct);//left motor will spin forward and change direction according to input from the right stick    
+        RightMotor.spin(vex::directionType::fwd, 30, vex::velocityUnits::pct);//left motor will spin forward and change direction according to input from the right stick
+  } 
+      else if (Controller.ButtonB.pressing()){
+        LeftMotor.spin(vex::directionType::fwd, 30, vex::velocityUnits::pct);//left motor will spin forward and change direction according to input from the right stick    
+        RightMotor.spin(vex::directionType::rev, 30, vex::velocityUnits::pct);//left motor will spin forward and change direction according to input from the right stick
+      } 
+
+    else{
+        LeftMotor.spin(vex::directionType::rev, 0, vex::velocityUnits::pct);//left motor will spin forward and change direction according to input from the right stick    
+        RightMotor.spin(vex::directionType::fwd, 0, vex::velocityUnits::pct);//left motor will spin forward and change direction according to input from the right stick
+
+    }
+  }
+
 }
